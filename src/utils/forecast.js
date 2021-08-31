9b187475b984023de941518675c423095ffabddb
loriel forelo <loriel@git.training>
2021-08-31 08:54:55 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,24 +1,31 @@
 const request = require("request");
+
 const forecast = (latitude, longitude, callback) => {
   const url =
-    "https://api.darkshy.net/forecast/9d1465c6f3bb7abc71944bddd8548d026" +
+    "https://api.darkshy.net/forecast/9d1465c6f3bb7abc71944bddd8548d026/" +
     latitude +
     "," +
     longitude;
 
-  request({ url: url, json: true }, (error, body) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to service!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
+      console.log(body.daily.data[0]);
       callback(
         undefined,
-        response.body.daily.data[0].summary +
-          " " +
+        body.daily.data[0].summary +
           " It is currently" +
           body.currently.temperature +
-          "% chance of rate."
+          "degress out. This high today is" +
+          body.daily.data[0].temperatureHigh +
+          "with a low of" +
+          body.daily.data[0].temperatureLow +
+          ". there is a " +
+          +body.currently.precipProbabilility +
+          "%"
       );
     }
   });
